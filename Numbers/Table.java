f7d6a9e84cbf36523e20eaab8170f674d7cfe0da
Madhuri3007 <madhurikallamadi@gmail.com>
2026-05-22 12:03:02 +0530

    Add files via upload

---
+++ Numbers/Table.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Table
+{
+    public static void main(String arg[])
+    {
+    int num=5;
+    
+    for(int i = 1 ; i <= 10 ; i++){
+        System.out.print(num + " * " + i + " = " + (num * i));
+    }
+}
+}
